17f43af89aaa6cd648f784a191cd5fa7fc1fcb38
SashaPlavi <sasha.bingulac@gmail.com>
2020-05-17 12:53:18 +0200

    cards done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      userName: '@nature',
+      content: 'I saw a neet tree!',
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      userName: '@nature',
+      content: 'I saw a snowy mountain!',
+    },
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/biking.jpeg',
+      userName: 'biking1222',
+      content: 'I did some biking today!',
+    },
+  ];
 }
